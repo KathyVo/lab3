9fe75e96efd12674dca931db11793f111a164648
Kathy Vo <kathyvo143@gmail.com>
2016-01-20 15:52:25 -0800

    javascript lab try 1

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,50 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("What happened???!!");
+		$('#testjs').text("YOLO");
+		$(".jumbotron p").toggleClass("active");
 	});
+
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(updateProject);
+
+}
+
+function updateProject(e) { //FIX LATER AT
+	var projectID = $("#project").val(); 
+	$(projectID).animate({
+		width: $('#width').val()
+	});
+	var newText = $("#description").val();
+	$(projectID + ".project-description").text(newText);
+}
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+function projectClick(e) {
+	//cancel default action to prevent page from reloading 
+	e.preventDefault(); 
+
+	//event listener to fire the event
+	var projectTitle = $(this).find("p").text(); 
+	var jumbotronHeader = $(".jumbotron h1"); 
+	console.log("Number of matching items " + jumbotronHeader.length);
+	jumbotronHeader.text(projectTitle);
+
+	//appending items 
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		containingProject.append("<div class = 'project-description'><p>Description of the Project</p></div>")
+
+	}
+	else {
+		$(".project-description").fadeOut();
+
+	}
+	
 }
+	
